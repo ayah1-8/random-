91c822abb6a0161834aab3332f0ff5a1bfc4778d
ayah karemah <ayaa20002010@hotmail.com>
2023-02-21 18:45:33 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [fact, setFact] = useState("");
+
+  const fetchFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) =>
+      setFact(res.data.fact)
+    );
+  };
+
+  useEffect(() => {
+    fetchFact();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchFact}>Generate Random Cat Fact!</button>
+      <p>{fact}</p>
     </div>
   );
 }
